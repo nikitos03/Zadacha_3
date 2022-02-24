d145cef53fa39003c0a7a286b0c83e4edd9c1c37
nikitos03 <83069842+nikitos03@users.noreply.github.com>
2022-02-24 13:12:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ЕНГ_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1234";
+            string vvodpassword;
+            int popitka = 3;
+            while(popitka > 0)
+            {
+                Console.WriteLine("Введите пароль: ");
+                vvodpassword = Console.ReadLine();
+                if (password == vvodpassword)
+                {
+                    Console.WriteLine("Секретное сообщение: Задача 3");
+                }
+                else
+                {
+                    popitka--;
+                    Console.WriteLine("Вы ввели неправильный пароль, у Вас осталось: " + popitka + " попытки что-бы ввести правильный пароль");
+                }
+            }
+            return;
+        }
+    }
+}
